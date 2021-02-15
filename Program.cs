a5f536c10bafd673742ddd40f1736c7216cb6220
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-02-15 10:46:12 +0100

    Added file parsing feature

--- Program.cs
+++ Program.cs
@@ -54,6 +54,11 @@ namespace GameOfLife
                         if (!enumerator.MoveNext() || !Int32.TryParse((string)enumerator.Current, out worldSize))
                             wrongUsage = true;
                         break;
+                    case "--file":
+                    case "-f":
+                        if (!enumerator.MoveNext() || !ParseWorldFile((string)enumerator.Current, out seed, out worldSize))
+                            wrongUsage = true;
+                        break;
                     case "--help":
                     case "-h":
                         PrintHelpAndExit(0);
@@ -79,15 +84,55 @@ Usage:
     {0} [OPTIONS] ..
                 
 Command Line Arguments:
-    -s, --seed<num>        : REQUIRED, The initial value of the world, written as a decimal number
-    -w, --world - size<num>  : Side length of the square world(Default 3)
-                
+    -f, --file <file>         : Parse seed and world size from file, containing rows of 0 and 1
+    -s, --seed <num>          : The initial value of the world, written as a decimal number
+    -w, --world-size <num>    : Side length of the square world(Default 3)
+
 Example:
+    {0} -f world.txt    : Parse seed and world size from 'world.txt'
     {0} -s 23 - w 4     : Creates a 4 x 4 world with the starting value of 23"
            , filename);
 
             Environment.Exit(returnCode);
         }
 
+        static bool ParseWorldFile(string filename, out BigInteger seed, out int worldSize)
+        {
+            seed = BigInteger.Zero;
+            worldSize = 0;
+
+            try
+            {
+                string data = File.ReadAllText(filename);
+
+                data = data.Replace("\r", "");
+                data = data.Replace("\n", "");
+
+                char[] rev = data.ToCharArray();
+                Array.Reverse(rev);
+                data = new string(rev);
+
+                int digits = 0;
+
+                foreach (char digit in data)
+                {
+                    seed <<= 1;
+                    seed += digit == '1' ? 1 : 0;
+                    digits++;
+                }
+
+                worldSize = (int)Math.Sqrt(digits);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                if (ex is FileNotFoundException || ex is DirectoryNotFoundException) Console.WriteLine("Error: File not found.");
+                return false;
+            }
+        }
+
+
     }
 }
+
