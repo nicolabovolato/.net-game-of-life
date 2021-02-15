7b21fd8f5884bc8ec0e67fbd935dc568b8d78a29
Nicola Bovolato <nicola.bovolato@gmail.com>
2021-02-15 07:54:33 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Numerics;
+using System.Threading;
+
+namespace GameOfLife
+{
+    class Program
+    {
+        static BigInteger seed;
+        static int worldSize = 3;
+
+        static void Main(string[] args)
+        {
+            ParseArgs();
+
+            World world = new World(seed, worldSize);
+
+            while (!world.Stable)
+            {
+                Console.Clear();
+                Console.WriteLine(world);
+
+                world.Advance();
+
+                Thread.Sleep(500);
+            }
+
+            Console.Write("World is stable.");
+        }
+
+        static void ParseArgs()
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            bool wrongUsage = false;
+
+            var enumerator = args.GetEnumerator();
+
+            enumerator.MoveNext(); //Skip the exe name;
+
+            while(enumerator.MoveNext())
+            {
+                string arg = (string)enumerator.Current;
+                switch (arg)
+                {
+                    case "--seed":
+                    case "-s":
+                        if (!enumerator.MoveNext() || !BigInteger.TryParse((string)enumerator.Current, out seed)) 
+                            wrongUsage = true;
+                        break;
+                    case "--world-size":
+                    case "-w":
+                        if (!enumerator.MoveNext() || !Int32.TryParse((string)enumerator.Current, out worldSize))
+                            wrongUsage = true;
+                        break;
+                    case "--help":
+                    case "-h":
+                        PrintHelpAndExit(0);
+                        break;
+                    default:
+                        wrongUsage = true;
+                        break;
+                }
+            }
+
+            if(seed == 0 || wrongUsage) PrintHelpAndExit(1);
+
+        }
+
+        static void PrintHelpAndExit(int returnCode)
+        {
+
+            string filename = Path.GetFileNameWithoutExtension(System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName);
+
+            Console.WriteLine(
+@"Conway's Game of life simulation
+Usage:
+    {0} [OPTIONS] ..
+                
+Command Line Arguments:
+    -s, --seed<num>        : REQUIRED, The initial value of the world, written as a decimal number
+    -w, --world - size<num>  : Side length of the square world(Default 3)
+                
+Example:
+    {0} -s 23 - w 4     : Creates a 4 x 4 world with the starting value of 23"
+           , filename);
+
+            Environment.Exit(returnCode);
+        }
+
+    }
+}
---
+++ World.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace GameOfLife
+{
+    class World
+    {
+        private int WorldSize { get; set; }
+
+        private BigInteger State { get; set; }
+
+        private List<BigInteger> _states = new List<BigInteger>();
+        private ReadOnlyCollection<BigInteger> States => _states.AsReadOnly();
+
+        public bool Stable { get; private set; }
+
+        private const int MAX_WORLD_SIZE = 50;
+        private const int UNDERPOPULATION_TRESHOLD = 2;
+        private const int OVERPOPULATION_TRESHOLD = 3;
+        private const int REPRODUCTION_TRIGGER = 3;
+
+        public World(BigInteger seed, int worldSize)
+        {
+            this.WorldSize = worldSize;
+            this.State = seed;
+            this._states.Add(State);
+
+            if (WorldSize > MAX_WORLD_SIZE) throw new Exception("World size exceeds maximum allowed.");
+            if (WorldSize < 1) throw new Exception("World size must be greater than 0.");
+
+        }
+
+        public void Advance()
+        {
+            if (Stable) return;
+
+            BigInteger newWorld = this.State;
+
+            for (int i = 0; i < Math.Pow(WorldSize, 2); i++)
+            {
+
+                int cellCount = CountNearbyCells(i);
+
+                int current = GetCell(i);
+
+                if (current == 1 && (cellCount < UNDERPOPULATION_TRESHOLD || cellCount > OVERPOPULATION_TRESHOLD))
+                {
+                    newWorld = newWorld ^ (BigInteger.One << i);
+                }
+                else if (current == 0 && cellCount == REPRODUCTION_TRIGGER)
+                {
+                    newWorld = newWorld | (BigInteger.One << i);
+                }
+            }
+
+            if (States.Contains(newWorld))
+            {
+                Stable = true;
+                return;
+            }
+
+            State = newWorld;
+
+            _states.Add(State);
+        }
+
+        private int CountNearbyCells(int index)
+        {
+            int cell_count = 0;
+
+            bool first_col = index % WorldSize == 0;
+            bool last_col = (index + 1) % WorldSize == 0;
+            bool first_row = index < WorldSize;
+            bool last_row = index > (Math.Pow(WorldSize, 2) - WorldSize);
+
+            if (!first_col) cell_count += GetCell(index - 1);
+            if (!last_col) cell_count += GetCell(index + 1);
+
+            if (!first_row)
+            {
+                cell_count += GetCell(index - WorldSize);
+                if (!first_col) cell_count += GetCell(index - WorldSize - 1);
+                if (!last_col) cell_count += GetCell(index - WorldSize + 1);
+            }
+
+            if (!last_row)
+            {
+                cell_count += GetCell(index + WorldSize);
+                if (!first_col) cell_count += GetCell(index + WorldSize - 1);
+                if (!last_col) cell_count += GetCell(index + WorldSize + 1);
+            }
+
+            return cell_count;
+        }
+
+        private int GetCell(int index)
+        {
+           return (State & BigInteger.Pow(2, index)) >> index == 1 ? 1 : 0;
+        }
+
+        public override string ToString()
+        {
+            string worldStr = "";
+
+            string spacer = new String('─', WorldSize * 2);
+
+            string openingStr = $"┌{spacer}┐\n";
+            string closingStr = $"└{spacer}┘";
+
+            for (int i = 0; i < Math.Pow(WorldSize, 2); i++)
+            {
+
+                if (i % WorldSize == 0)
+                {
+                    worldStr += "|";
+                }
+
+                int current = GetCell(i);
+
+                worldStr += current == 1 ? "██" : "  ";
+
+                if ((i + 1) % WorldSize == 0)
+                {
+                    worldStr += "|";
+                    worldStr += "\n";
+                }
+            }
+
+            return openingStr + worldStr + closingStr;
+        }
+    }
+}
